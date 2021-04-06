968d42a6f5dfef3f6864ec0a5c2d5819960c1e5c
Mateus Varelo <mateusferreira703@gmail.comgit config --global user.name Mateus>
2021-04-06 14:43:46 -0300

    Implementando mensagem de usuarios na tela

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from view import PokedexView
 
 print("Digite um nome de um pokémon.")
+print("Ou se preferir digite um número entre 1 é 1118 que corresponde a um pokémon.")
+nome_numero = input()
+print("Qual tipo de arquivo (CSV)ou(JSON):")
+tipo = input()
+pokemon = PokedexView()
+pokemon.pesquisar(nome_numero,tipo)
+
+print(pokemon.resposta())
