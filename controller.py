578184c6f97d7825d344c56b57ec4d7200811fef
Mateus Varelo <mateusferreira703@gmail.comgit config --global user.name Mateus>
2021-04-06 13:31:36 -0300

    lógica de controle de arquivos

--- controller.py
+++ controller.py
@@ -5,4 +5,13 @@ class PokedexControl():
         self.poke_model = PokedexModel()
         
     def request_type(self,name_pokemon,type_file):
+        type_file = []
+        if (type_file == "csv"):
+            type_file = self.poke_model.request_csv(name_pokemon) 
+        
+        elif (type_file == "json"):
+            type_file = self.poke_model.request_json(name_pokemon)  
+            
+              
+              
        
