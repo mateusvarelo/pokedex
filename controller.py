92dcd77c65d9cae182da54d408ab079c4b218a02
Luzivan-lira <luzivanlira@gmail.com>
2021-04-08 17:54:48 -0300

    Implementado requisição pelo tipo no arquivo controller

--- controller.py
+++ controller.py
@@ -4,5 +4,11 @@ class PokedexControl():
     def __init__(self):
         self.poke_model = PokedexModel()
         
-    def request_type(self,name_pokemon,type_file):
+    def request_type(self,metodos_encontro,type_file):
+        conteudo = []
+        if (type_file == 1): 
+             conteudo = self.poke_model.solicitar_csv(metodos_encontro)
+        elif (type_file == 2):
+             conteudo = self.poke_model.solicitar_json(metodos_encontro)
+        return conteudo
        
